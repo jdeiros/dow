c29473fffb6e27472468190b74d78a056d4a8775
Jerónimo Deiros <jeronimodeiros@gmail.com>
2020-10-24 00:27:56 -0300

    login and register UI

--- app/src/main/java/com/unlam/dow/MainActivity.java
+++ app/src/main/java/com/unlam/dow/MainActivity.java
@@ -2,13 +2,41 @@ package com.unlam.dow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mButtonWalker;
+    Button mButtonOwner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonWalker = findViewById(R.id.btnWalker);
+        mButtonOwner = findViewById(R.id.btnOwner);
+
+        mButtonOwner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSelectAuth();
+            }
+        });
+
+        mButtonWalker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSelectAuth();
+            }
+        });
+    }
+
+    private void goToSelectAuth() {
+        Intent intent = new Intent(MainActivity.this, SelectOptionAuthActivity.class);
+        startActivity(intent);
     }
 }
